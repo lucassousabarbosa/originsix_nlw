df5e939b68b0579864e65bb02b34f0f0f633b374
lucassousabarbosa <lucas121201@outlook.com>
2021-09-24 13:14:43 -0300

    update bluehatteam

--- assents/js/app.js
+++ assents/js/app.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('nav .toggle');
 const links = document.querySelectorAll('nav ul li a');
+const header = document. querySelector('#header');
+const scrollHeight = header.offsetHeight;
 
 for (const element of toggle) {
   element.addEventListener('click', function() {
@@ -10,6 +12,14 @@ for (const element of toggle) {
 
 for (const link of links) {
   link.addEventListener('click', function() {
-    nav.classList.remove('show')
+    nav.classList.remove('show');
   })
 }
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= scrollHeight) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
+})
