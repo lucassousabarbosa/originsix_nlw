c699e4862fb33ffddc9e70175786d3f0022e71ad
lucassousabarbosa <lucas121201@outlook.com>
2021-09-25 23:43:56 -0300

    add icons and testemonials

--- assents/js/app.js
+++ assents/js/app.js
@@ -23,3 +23,14 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scroll');
   }
 })
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
